614595018c7ff9109aa852bac9217bc61e829f61
bezdziel <bezdziel@gmail.com>
2017-01-15 21:07:46 +0100

    Remove unused references

--- Bookstore.Core/SearchEngine.cs
+++ Bookstore.Core/SearchEngine.cs
@@ -1,10 +1,5 @@
-﻿using Bookstore.Core.Model;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Bookstore.Core
--- Bookstore.Provider.Amazon/Model/AmazonBook.cs
+++ Bookstore.Provider.Amazon/Model/AmazonBook.cs
@@ -1,17 +1,9 @@
 ﻿using Bookstore.Core;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Serialization;
 
 namespace Bookstore.Provider.Model
 {
-  
     public class AmazonBook : IBook
     {
-
         public string Isbn { get; set; }
         public decimal Price { get; set; }
         public string Title { get; set; }
